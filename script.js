218ef9d85bb370cef4713e58107e6abf3474dc4a
olena-olena <vykaliuk@gmail.com>
2020-05-21 23:47:41 +0300

    select joke type and categories

--- script.js
+++ script.js
@@ -9,21 +9,24 @@ const radioRandom = document.getElementById('random');
 const categoriesList = document.getElementById('categories-list');
 const joke = document.querySelector('p');
 
+
 fetch(urlCategories)
   .then(response => response.json())
   .then(data => {
     categoriesList.innerHTML = '';
     for (let i in data) {
+      let cat = document.createElement('label');
+      cat.for = data[i];
+      cat.innerHTML = data[i];
+      categoriesList.appendChild(cat);
+
       let box = document.createElement('input');
       box.type = 'checkbox';
-      box.id = `${data[i]}`;
-      box.value = `${data[i]}`;
-      box.style = "visibility: hidden"
+      box.id = data[i];
+      box.value = data[i];
+      // box.style = "visibility: hidden
+      cat.appendChild(box);
 
-      let cat = document.createElement('label');
-      cat.innerHTML = `${data[i]}`;
-
-      categoriesList.appendChild(cat).appendChild(box);
     }
   });
 
@@ -33,18 +36,43 @@ radioRandom.addEventListener('change', changeCategory);
 
 
 function changeCategory(event) {
-  // if (event.target.id == 'random') {
-  categoriesList.classList.toggle('hidden')
-
+  // if (event.target.id == 'random') {}
+  categoriesList.classList.toggle('hidden');
 }
 
-function getJoke(url) {
+function getRandomJoke(url=urlRandom) {
   fetch(url)
     .then(response => response.json())
     .then(data => {joke.innerHTML = data.value})
 };
 
+function getCategoryJoke(lst) {
+  console.log(lst);
+}
+
+
+
 function formSubmit(event) {
-  getJoke(urlRandom);
   event.preventDefault();
+
+  let inputs = [];
+  let checks = [];
+  let reqCategories = [];
+
+  for (input of event.target.querySelectorAll('input')) { //tegNAme
+    // console.log(input.id, input.checked);
+    inputs.push(input.id);
+    checks.push(input.checked);
+  }
+
+  if (checks[0]) {
+    getRandomJoke();
+  } else if (checks[1]) {
+    for (var i = 2; i < (inputs.length - 1); i++) {
+      if (checks[i]) {
+        reqCategories.push(inputs[i]);
+      }
+    }
+    getCategoryJoke(reqCategories);
+  }
 };
